1e94bc7334d722120814bd9a37cba43142b74cc3
Meta-c <metamedhat116@gmail.com>
2022-10-04 10:42:35 +0200

    added devie preview

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'input_page.dart';
 import 'result_page.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() => runApp(BMICalculator());
+void main() => runApp(DevicePreview(
+    enabled: !kReleaseMode, builder: (context) => BMICalculator()));
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData(
         primaryColor: Colors.red,
         backgroundColor: const Color(0xFF0A0E21),
